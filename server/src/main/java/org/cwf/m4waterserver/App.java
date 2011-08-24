43d6d2f5924ea307e072a91254a085f5e34e843c
vkakama <vkakama@gmail.com>
2011-08-24 13:51:21 +0000

    created server project

---
+++ server/src/main/java/org/cwf/m4waterserver/App.java
@@ -0,0 +1,13 @@
+package org.cwf.m4waterserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
