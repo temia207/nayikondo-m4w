d5f75d55d52e6b076a8b3302e580095c1a29b0a3
kayondor <kayondor@gmail.com>
2011-08-27 13:38:51 +0000

    Added ticket custom service

--- server/src/main/java/org/m4water/server/yawl/TicketYawlService.java
+++ server/src/main/java/org/m4water/server/yawl/TicketYawlService.java
@@ -1,10 +1,39 @@
 
 package org.m4water.server.yawl;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+import org.yawlfoundation.yawl.engine.interfce.WorkItemRecord;
+import org.yawlfoundation.yawl.engine.interfce.interfaceB.InterfaceBWebsideController;
+
 /**
  *
  * @author kay
  */
-public class TicketYawlService {
+@Component("yawlTicketService")
+public class TicketYawlService extends InterfaceBWebsideController implements  InitializingBean{
+
+        private static  TicketYawlService ticketYawlService;
+
+        public static TicketYawlService getInstance(){
+           return ticketYawlService;
+        }
+
+        @Override
+        public void handleEnabledWorkItemEvent(WorkItemRecord enabledWorkItem) {
+              
+        }
+
+        @Override
+        public void handleCancelledWorkItemEvent(WorkItemRecord workItemRecord) {
+              
+        }
+
+
+        @Override
+        public void afterPropertiesSet() throws Exception {
+                System.out.print("Initiations the yawl custom service");
+               ticketYawlService = this;
+        }
 
 }
---
+++ server/src/main/java/org/m4water/server/yawl/YawlUtil.java
@@ -0,0 +1,10 @@
+
+package org.m4water.server.yawl;
+
+/**
+ *
+ * @author kay
+ */
+public class YawlUtil {
+
+}
