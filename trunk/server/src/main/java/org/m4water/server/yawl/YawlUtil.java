8e8efa3e10eacf9dd6fddc223038a2c278551b1a
kayondor@gmail.com <kayondor@gmail.com@1b24c0ac-4e45-cc42-765a-2c42507c4f44>
2011-08-27 15:28:13 +0000

    Adding yawl code

--- trunk/server/src/main/java/org/m4water/server/yawl/YawlUtil.java
+++ trunk/server/src/main/java/org/m4water/server/yawl/YawlUtil.java
@@ -1,10 +1,39 @@
 
 package org.m4water.server.yawl;
 
+import java.util.List;
+import org.yawlfoundation.yawl.engine.interfce.WorkItemRecord;
+import org.yawlfoundation.yawl.engine.interfce.interfaceB.InterfaceBWebsideController;
+
 /**
  *
  * @author kay
  */
 public class YawlUtil {
+
+//        public static WorkItemRecord checkOut(InterfaceBWebsideController controller , WorkItemRecord wir){
+//                controller.checkOut(null, null)
+//        }
+//
+//        public static String initSessionHandle(InterfaceBWebsideController controller, String sessionHandle){
+//
+//
+//
+//        }
+//
+//         private List<WorkItemRecord> checkOutChildren(InterfaceBWebsideController intfcB,String workItemId){
+//                                 initSessionHandle();
+//
+//                List<WorkItemRecord> children = getChildren(workItemId, _sessionHandle);
+//
+//                for (int i = 0; i < children.size(); i++) {
+//
+//                        WorkItemRecord itemRecord = (WorkItemRecord) children.get(i);
+//                        if (WorkItemRecord.statusFired.equals(itemRecord.getStatus())) {
+//                                checkOut(itemRecord.getID(), _sessionHandle);
+//                        }
+//                }
+//                return children;
+//         }
 
 }
