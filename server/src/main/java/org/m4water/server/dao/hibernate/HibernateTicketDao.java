5602d5bf42bf3b7c42efab460845111b793d5860
vkakama <vkakama@gmail.com>
2011-08-28 10:28:18 +0000

    edited TicketSms to insert real data

--- server/src/main/java/org/m4water/server/dao/hibernate/HibernateTicketDao.java
+++ server/src/main/java/org/m4water/server/dao/hibernate/HibernateTicketDao.java
@@ -2,24 +2,33 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.m4water.server.dao.hibernate;
+
 import com.googlecode.genericdao.search.Search;
 import java.util.List;
+import net.sf.beanlib.hibernate3.Hibernate3BeanReplicator;
+import org.hibernate.Hibernate;
 import org.m4water.server.admin.model.Ticket;
+import org.m4water.server.admin.model.Waterpoint;
 import org.m4water.server.dao.TicketDao;
 import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author victor
  */
 @Repository("Ticket")
-public class HibernateTicketDao extends BaseDAOImpl<Ticket> implements TicketDao{
+public class HibernateTicketDao extends BaseDAOImpl<Ticket> implements TicketDao {
 
     @Override
     public List<Ticket> getTickets() {
         Search s = new Search();
-		s.addSort("id", false);
+        s.addSort("id", false);
+//        final List<Ticket> search = search(s);
+//        Ticket get = search.get(0);
+//        Hibernate.initialize(get.getWaterpoint());
+//        Waterpoint waterpoint = get.getWaterpoint();
+
         return search(s);
     }
 
@@ -37,5 +46,4 @@ public class HibernateTicketDao extends BaseDAOImpl<Ticket> implements TicketDao
     public void saveTicket(Ticket ticket) {
         save(ticket);
     }
-
 }
