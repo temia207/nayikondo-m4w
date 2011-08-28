94cee6fc597078e07bb185f1e5ee2eedd63dee1e
kayondor <kayondor@gmail.com>
2011-08-28 09:58:07 +0000

    Adding InterfaceBHelper to help manage workItems

--- server/src/main/java/org/m4water/server/yawl/TicketYawlService.java
+++ server/src/main/java/org/m4water/server/yawl/TicketYawlService.java
@@ -1,9 +1,15 @@
 package org.m4water.server.yawl;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openxdata.yawl.util.InterfaceBHelper;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 import org.yawlfoundation.yawl.engine.interfce.WorkItemRecord;
 import org.yawlfoundation.yawl.engine.interfce.interfaceB.InterfaceBWebsideController;
+import org.yawlfoundation.yawl.exceptions.YAWLException;
 
 /**
  *
@@ -13,6 +19,7 @@ import org.yawlfoundation.yawl.engine.interfce.interfaceB.InterfaceBWebsideContr
 public class TicketYawlService extends InterfaceBWebsideController implements InitializingBean {
 
         private static TicketYawlService ticketYawlService;
+        private InterfaceBHelper yawlHelper;
 
         public static TicketYawlService getInstance() {
                 return ticketYawlService;
@@ -20,6 +27,16 @@ public class TicketYawlService extends InterfaceBWebsideController implements In
 
         @Override
         public void handleEnabledWorkItemEvent(WorkItemRecord enabledWorkItem) {
+                try {
+                        yawlHelper.initSessionHandle();
+                        List<WorkItemRecord> workitems = yawlHelper.checkOutWorkitemAndChildren(enabledWorkItem);
+
+
+                } catch (YAWLException ex) {
+                        Logger.getLogger(TicketYawlService.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                        Logger.getLogger(TicketYawlService.class.getName()).log(Level.SEVERE, null, ex);
+                }
         }
 
         @Override
@@ -29,6 +46,7 @@ public class TicketYawlService extends InterfaceBWebsideController implements In
         @Override
         public void afterPropertiesSet() throws Exception {
                 System.out.println("Initiations the yawl custom service");
+                yawlHelper = new InterfaceBHelper(this, _interfaceBClient, DEFAULT_ENGINE_USERNAME, DEFAULT_ENGINE_PASSWORD);
                 ticketYawlService = this;
         }
 }
