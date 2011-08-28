98c0826ece4939d63b872d978bbd85612e3ed231
kayondor@gmail.com <kayondor@gmail.com@1b24c0ac-4e45-cc42-765a-2c42507c4f44>
2011-08-28 13:41:37 +0000

    Workflow are now launched Ticketes are given random IDs

--- trunk/server/src/main/java/org/m4water/server/service/impl/YawlServiceImpl.java
+++ trunk/server/src/main/java/org/m4water/server/service/impl/YawlServiceImpl.java
@@ -1,19 +1,35 @@
 package org.m4water.server.service.impl;
 
 import org.m4water.server.admin.model.Ticket;
+import org.m4water.server.service.YawlService;
 import org.m4water.server.yawl.TicketYawlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author kay
  */
-public class YawlServiceImpl {
+@Service("yawlService")
+public class YawlServiceImpl implements YawlService {
 
         @Autowired
         TicketYawlService yawlService;
+        private static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(YawlServiceImpl.class);
 
+        @Override
         public void launchWaterPointFlow(Ticket ticket) {
-               
+                try {
+                        TicketYawlService.Params params = new TicketYawlService.Params();
+                        params.addPumpMechanicName("mechanic");
+                        params.setSenderNumber(ticket.getCreatorTel());
+                        params.setMechanicNumber("0789388969");
+                        params.setWaterPointID(ticket.getWaterpoint().getReferenceNumber());
+                        params.setTicketMessage(ticket.getMessage());
+                        yawlService.launchCase(params);
+                } catch (Exception ex) {
+                        log.error("Error occured while launching a ticket workflow", ex);
+                        ex.printStackTrace();
+                }
         }
 }
