cd7bdea4cd9dbfc64822d24bd406642b1b6651d4
kayondor <kayondor@gmail.com>
2011-09-12 09:17:39 +0000

    Resolving conflicts

--- server/src/main/java/org/m4water/server/sms/TicketSms.java
+++ server/src/main/java/org/m4water/server/sms/TicketSms.java
@@ -41,125 +41,146 @@ import org.springframework.transaction.support.TransactionTemplate;
 @Transactional
 public class TicketSms implements TicketService, InitializingBean {
 
-    @Autowired
-    private WaterPointDao waterPointDao;
-    @Autowired
-    private ProblemDao problemDao;
-    private TransactionTemplate transactionTemplate;
-    @Autowired
-    private PlatformTransactionManager transactionManager;
-    @Autowired
-    private SessionFactory session;
-    @Autowired
-    private YawlService yawlService;
-    @Autowired
-    private SMSServiceImpl smsService;
+        @Autowired
+        private WaterPointDao waterPointDao;
+        @Autowired
+        private ProblemDao problemDao;
+        private TransactionTemplate transactionTemplate;
+        @Autowired
+        private PlatformTransactionManager transactionManager;
+        @Autowired
+        private SessionFactory session;
+        @Autowired
+        private YawlService yawlService;
+        @Autowired
+        private SMSServiceImpl smsService;
 
-    public TicketSms() {
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        int numOfProcessors = 10;
-        System.out.println("starting sms server");
-        ModemGateway gateWay = new SerialModemGateway("modem.com1", "COM35", 460200, "Nokia", "6500c");
-        transactionTemplate = new TransactionTemplate(transactionManager);
-        Channel ch = new ModemChannel(gateWay);
-        // SMSServer s = new SMSServer(ch, new RequestListenerImpl(), numOfProcessors);
-
-                transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-
-                    @Override
-                    protected void doInTransactionWithoutResult(TransactionStatus status) {
-                        final Problem problem = new Problem();
-                        Date date = new Date();
-                        problem.setDateProblemReported(date);
-                        problem.setProblemDescsription("smelling water");
-                        problem.setProblemStatus("F");
-                        // final Waterpoint waterPoint = waterPointDao.getWaterPoint("UMASA0123");
-                        final Waterpoint waterPoint = waterPointDao.getWaterPoint("405BHS321D");
-                        session.getCurrentSession().evict(waterPoint);
-                        problem.setWaterpoint(waterPoint);
-                        waterPoint.setProblems(new HashSet());
-                        waterPoint.getProblems().add(problem);
-                        //save problem log
-                        ProblemLog log = new ProblemLog();
-                        log.setDate(date);
-                        log.setSenderNo("0714505033");
-                        log.setIssue("amazi gawunya");
-                        log.setProblem(problem);
-                        problem.setProblemLogs(new HashSet());
-                        problem.getProblemLogs().add(log);
-                        problemDao.save(problem);
-                        launchCase(problem);
-                    }
-                });
-
-
-        SMSServer server = new SMSServer(ch, new RequestListener() {
-
-            @Override
-            public void processRequest(final SMSMessage request) {
-                System.out.println("new message " + request.getSmsData());
-                request.getSmsData();
-                String msg = request.getSmsData();
-                final String sourceId = msg.split(" ")[0];
-                final String complaint = msg.substring(sourceId.length());
-
-                transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-
-                    @Override
-                    protected void doInTransactionWithoutResult(TransactionStatus status) {
-                        final Problem problem = new Problem();
-                        Date date = new Date();
-                        problem.setDateProblemReported(date);
-                        problem.setProblemDescsription(complaint);
-                        problem.setProblemStatus("F");
-                        // final Waterpoint waterPoint = waterPointDao.getWaterPoint("UMASA0123");
-                        final Waterpoint waterPoint = waterPointDao.getWaterPoint(sourceId);
-                        session.getCurrentSession().evict(waterPoint);
-                        problem.setWaterpoint(waterPoint);
-                        waterPoint.setProblems(new HashSet());
-                        waterPoint.getProblems().add(problem);
-                        problemDao.save(problem);
-                        launchCase(problem);
-                    }
-                });
-
-
-            }
-        }, numOfProcessors);
-
-        try {
-            server.startServer();
-        } catch (Exception ex) {
-            Logger.getLogger(TicketSms.class.getName()).log(Level.SEVERE, null, ex);
+        public TicketSms() {
         }
-    }
 
-    @Override
-    @Transactional(readOnly = true)
-    public Problem getProblem(int id) {
-        return problemDao.getProblem(id);
-    }
+        @Override
+        public void afterPropertiesSet() throws Exception {
+                int numOfProcessors = 10;
+                System.out.println("starting sms server");
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<Problem> getProblems() {
-        return problemDao.getProblems();
-    }
+                ModemGateway gateWay = new SerialModemGateway("modem.com1", "COM21", 460200, "Nokia", "6500c");
 
-    @Override
-    public void saveProblem(Problem problem) {
-        problemDao.save(problem);
-    }
+                transactionTemplate = new TransactionTemplate(transactionManager);
+                Channel ch = new ModemChannel(gateWay);
+                // SMSServer s = new SMSServer(ch, new RequestListenerImpl(), numOfProcessors);
 
-    @Override
-    public void deleteProblem(Problem problem) {
-        problemDao.deleteProblem(problem);
-    }
+                transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 
-    public void launchCase(Problem problem) {
-        yawlService.launchWaterPointFlow(problem);
-    }
+                        @Override
+                        protected void doInTransactionWithoutResult(TransactionStatus status) {
+                                final Problem problem = new Problem();
+                                Date date = new Date();
+                                problem.setDateProblemReported(date);
+                                problem.setProblemDescsription("smelling water");
+                                problem.setProblemStatus("F");
+                                // final Waterpoint waterPoint = waterPointDao.getWaterPoint("UMASA0123");
+                                final Waterpoint waterPoint = waterPointDao.getWaterPoint("405BHS321D");
+                                session.getCurrentSession().evict(waterPoint);
+                                problem.setWaterpoint(waterPoint);
+                                waterPoint.setProblems(new HashSet());
+                                waterPoint.getProblems().add(problem);
+                                //save problem log
+                                ProblemLog log = new ProblemLog();
+                                log.setDate(date);
+                                log.setSenderNo("0714505033");
+                                log.setIssue("amazi gawunya");
+                                log.setProblem(problem);
+                                problem.setProblemLogs(new HashSet());
+                                problem.getProblemLogs().add(log);
+                                problemDao.save(problem);
+                                launchCase(problem);
+                        }
+                });
+
+
+                SMSServer server = new SMSServer(ch, new RequestListener() {
+
+                        @Override
+                        public void processRequest(final SMSMessage request) {
+                                System.out.println("new message " + request.getSmsData());
+                                request.getSmsData();
+                                String msg = request.getSmsData();
+                                final String sourceId = msg.split(" ")[0];
+                                final String complaint = msg.substring(sourceId.length());
+
+                                transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+                                        @Override
+                                        protected void doInTransactionWithoutResult(TransactionStatus status) {
+                                                try {
+                                                        final Problem problem = new Problem();
+                                                        Date date = new Date();
+                                                        problem.setDateProblemReported(date);
+                                                        problem.setProblemDescsription(complaint);
+                                                        problem.setProblemStatus("F");
+                                                        // final Waterpoint waterPoint = waterPointDao.getWaterPoint("UMASA0123");
+                                                        final Waterpoint waterPoint = waterPointDao.getWaterPoint(sourceId);
+                                                        if (waterPoint == null) {
+                                                                smsService.sendSMS(request.getSender(), "Water Point Id Not Found");
+                                                                return;
+                                                        }
+                                                        session.getCurrentSession().evict(waterPoint);
+                                                        problem.setWaterpoint(waterPoint);
+                                                        waterPoint.setProblems(new HashSet());
+                                                        waterPoint.getProblems().add(problem);
+
+                                                        problem.setProblemLogs(new HashSet() {
+
+                                                                {
+                                                                        add(new ProblemLog(0, problem, request.getSender(), new Date(), waterPoint.getWaterpointId()));
+                                                                }
+                                                        });
+
+                                                        problemDao.save(problem);
+
+                                                        launchCase(problem);
+                                                } catch (Throwable x) {
+                                                        smsService.sendSMS(request.getSender(), "Invalid Message send message in Format: "
+                                                                + "<waterpointid> <message>");
+                                                }
+                                        }
+                                });
+
+
+                        }
+                }, numOfProcessors);
+
+                try {
+                        server.startServer();
+                } catch (Exception ex) {
+                        Logger.getLogger(TicketSms.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        }
+
+        @Override
+        @Transactional(readOnly = true)
+        public Problem getProblem(int id) {
+                return problemDao.getProblem(id);
+        }
+
+        @Override
+        @Transactional(readOnly = true)
+        public List<Problem> getProblems() {
+                return problemDao.getProblems();
+        }
+
+        @Override
+        public void saveProblem(Problem problem) {
+                problemDao.save(problem);
+        }
+
+        @Override
+        public void deleteProblem(Problem problem) {
+                problemDao.deleteProblem(problem);
+        }
+
+        public void launchCase(Problem problem) {
+
+                yawlService.launchWaterPointFlow(problem);
+        }
 }
