8ad76d89f20060d9c25d295df70b554929478137
kayondor@gmail.com <kayondor@gmail.com@1b24c0ac-4e45-cc42-765a-2c42507c4f44>
2011-10-22 23:58:58 +0000

    Sms Validation plus hibernate fix which threw a cascade-delete-orphan related exception

--- trunk/server/src/main/java/org/m4water/server/sms/TicketSms.java
+++ trunk/server/src/main/java/org/m4water/server/sms/TicketSms.java
@@ -90,16 +90,13 @@ public class TicketSms implements TicketService, InitializingBean {
         problem.setProblemDescsription(complaint);
         problem.setProblemStatus("open");
         problem.setWaterpoint(waterPoint);
-        problem.setProblemLogs(new HashSet());
-
-        waterPoint.setProblems(new HashSet());
         waterPoint.getProblems().add(problem);
 
         ProblemLog problemLog = new ProblemLog(UUID.jUuid(), problem, sender, date, waterPoint.getWaterpointId());
         problem.getProblemLogs().add(problemLog);
-        launchCase(problem);
+//        launchCase(problem);
         saveProblem(problem);
-        
+
     }
 
     private void processSms(final SMSMessage request) throws TransactionException {
@@ -144,11 +141,12 @@ public class TicketSms implements TicketService, InitializingBean {
     }
 
     public void processMessage(String sourceId, String complaint, String sender) throws HibernateException {
+
         Waterpoint waterPoint = waterPointDao.getWaterPoint(sourceId);
-        if (waterPoint == null) {
+        if (waterPoint == null && !validWaterPointID(sourceId)) {
+            smsService.sendSMS(sender, "Invalid Waterpoint ID(" + sourceId + ") Format. Form should be similar to 521BH001 with 8 characters");
+        } else if (waterPoint == null) {
             smsService.sendSMS(sender, "Waterpoint ID(" + sourceId + ") does not exist. Please send again with correct ID");
-        } else if (validWaterPointID(sourceId)) {
-            smsService.sendSMS(sender, "Invalid Waterpoint ID(" + sourceId + ") Format. Form should be similar to 521PZ001 with 8 characters");
         } else if (waterPoint.hasOpenProblems()) {
             Problem problem = waterPoint.getOpenProblem();
             ProblemLog problemLog = new ProblemLog(UUID.jUuid(), problem, sender, new Date(), complaint);
@@ -267,8 +265,14 @@ public class TicketSms implements TicketService, InitializingBean {
         messageLogDao.save(new Smsmessagelog(java.util.UUID.randomUUID().toString(), request.get("msgID") + "", request.getSender(), request.get("time") + "", request.getSmsData()));
     }
 
-    private boolean validWaterPointID(String sourceId) {
+    private static boolean validWaterPointID(String sourceId) {
         return sourceId != null
-                && "[0-9]{3}[a-zA-Z]{2}[0-9]{3}".matches(sourceId);
+                && sourceId.matches("[0-9]{3}[a-zA-Z]{2}[0-9]{3}");
+    }
+
+    public static void main(String[] args) {
+        String string = "mabura";
+        System.out.println("Match for: " + string + " =  " + validWaterPointID(string));
+
     }
 }
